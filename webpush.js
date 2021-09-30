65db8137c5e9e4312c4a7359d8241436efd97b53
Momin Ali <momin1998.m@gmail.com>
2021-09-30 07:12:31 +0600

    fix

--- webpush.js
+++ webpush.js
@@ -1,11 +1,12 @@
 const webpush = require('web-push');
 
-// let vapidKeys = webpush.generateVAPIDKeys();
+//let vapidKeys = webpush.generateVAPIDKeys();
+//console.log(vapidKeys);
 // VAPID keys should only be generated only once.
 const vapidKeys = {
   publicKey:
-    'BBZY7Q3KEtZArAAWMLi_qzWHbH4vAoqPpIXnRhmlUaw0PVs1Kt_2fgLhuaVI5i8MWASBKx3d6W6UoH2U3qChw9U',
-  privateKey: 'CZtf_JUxmXkCKbzwaKedPPO9BFC99U2rk-GUYDbYAa8'
+    'BGxoxpakbEPh1xT3LO7xFFtaHlmicfXX0FHuWNX4buDm7zEi7DoXe9GKC8KNSJXqnWdoT8_2Oo-uv40Q8yyTScE',
+  privateKey: 'i1O0viWe1jyIa1LVmKISJUgFupy9BL08UujM7CDj0OQ'
 };
 
 webpush.setVapidDetails(
@@ -14,13 +15,24 @@ webpush.setVapidDetails(
   vapidKeys.privateKey
 );
 
-// This is the same output of calling JSON.stringify on a PushSubscription
-const pushSubscription = {
-  endpoint: '.....',
-  keys: {
-    auth: '.....',
-    p256dh: '.....'
-  }
-};
+const payload = JSON.stringify({
+  body: 'Here is a notification body!',
+  icon: 'https://apsissolutions.com/wp-content/uploads/2021/02/APSIS-Solutions-Header-Logo.png',
+  vibrate: [100, 50, 100],
+  data: {
+    dateOfArrival: Date.now(),
+    primaryKey: 1
+  },
+  actions: [
+    {action: 'explore', title: 'Explore this new world',
+      icon: 'images/checkmark.png'},
+    {action: 'close', title: 'Close notification',
+      icon: 'images/xmark.png'},
+  ]
+});
 
-webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
+// This is the same output of calling JSON.stringify on a PushSubscription
+const pushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/d9TKlMpNN8g:APA91bEN__AI_JUXseRSFVj7N5qQ9Gij_8V6IMdJEJMLFY5BokVvtF5fUU2qevfWzwr9Jk0lw_9MZXucKKbNqr7c3V_zUw7xS3Dc8rWGRvHlAnpquqpbghDgLntQIV3t1Fpk-b1ILfUT","expirationTime":null,"keys":{"p256dh":"BA6X9TJhwqWFfh2CDa8lbu7U3LK4E7eK4NtayF4yUIhGJRiluklIHHQ85KvFmsKaNTQSHfHnXKsd2gmNx8tOrSs","auth":"LEbRY-pJGwmWvPm1s-flLQ"}};
+
+
+webpush.sendNotification(pushSubscription, payload);
